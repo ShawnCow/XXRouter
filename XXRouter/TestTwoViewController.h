e6857f225cf9f43ad6423608cbf0cb071ae08830
黄修勇 <huangxiuyong@yhd.com>
2017-05-12 10:58:30 +0800

    Revert "Revert "添加跳转的栈优化""
    This reverts commit 61c3ac79ac29eb3e2086a3af13de1a8988db1eed.

---
+++ XXRouter/TestTwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTwoViewController.h
+//  XXRouter
+//
+//  Created by Shawn on 2017/5/12.
+//  Copyright © 2017年 Shawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTwoViewController : UIViewController
+
+@end
